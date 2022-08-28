370aa3d8917cfd5ad97b00adf70dcea7ac822852
Leila Badra <leila.badra@hotmail.com>
2022-08-28 20:29:12 +0200

    Done

---
+++ src/components/Message.vue
@@ -0,0 +1,14 @@
+<template>
+        <p className="message">
+         {{message}}
+        </p>
+</template>
+
+  <script>
+  export default {
+    props: {   
+      message:String,
+    },  
+  }
+  </script>
+  
